c175abde36b58fec8231de7576d742a27d9b65c0
zchen661 <zchen661@gatech.edu>
2021-02-14 20:53:06 -0500

    made the create manifest screen

---
+++ app/src/main/java/com/example/myapplication/ManifestList.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManifestList extends AppCompatActivity {
+
+    ListView lst;
+    String[] manifest = new String[]{"Manifest A", "Manifest B", "Manifest C", "Manifest D", "Manifest E", "Manifest F"};
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manifest_list);
+        this.setTitle("Manifests");
+        lst = (ListView) findViewById(R.id.listview);
+        ManifestListFormat customListview = new ManifestListFormat(this, manifest);
+        lst.setAdapter(customListview);
+
+        /* intent setup
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //String mview = manifest[position].toString();
+                Intent intent = new Intent(ManifestList.this, MainActivity.class);
+                //intent.putExtra("Listviewclickvalue", mview);
+                startActivity(intent);
+            }
+        });
+         */
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ManifestListFormat.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ManifestListFormat extends ArrayAdapter<String> {
+
+    private String[] manifest;
+    private Integer[] imgid;
+    private Activity context;
+
+    public ManifestListFormat(Activity context, String[] manifest) {
+        super(context, R.layout.activity_manifest_list_format, manifest);
+
+        this.context = context;
+        this.manifest = manifest;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        ViewHolder viewHolder = null;
+        if (r == null)
+        {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.activity_manifest_list_format, null, true);
+            viewHolder = new ViewHolder(r);
+            r.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) r.getTag();
+        }
+
+        viewHolder.tvw1.setText(manifest[position]);
+
+        return r;
+    }
+
+    class ViewHolder
+    {
+        TextView tvw1;
+        ViewHolder(View v)
+        {
+            tvw1 = (TextView) v.findViewById(R.id.manifest);
+        }
+    }
+}
